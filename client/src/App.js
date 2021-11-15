72029b8e78cb10ee5e653559fcd3eccbc09af02d
Brian Zhao <bzhao1991@gmail.com>
2021-11-15 17:52:33 -0500

    adds header.js to component folder for real this time

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import "./App.scss";
-import NotFoundPage from "./pages/NotFoundPage";
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFountPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -8,8 +10,8 @@ function App() {
       <Router>
         <header className="App-header"></header>
         <Switch>
-          <Route path="/" exact component={Homepage} />
-          <Route path="/inventory/" component={Inventory} />
+          <Route path="/" exact component={WarehousePage} />
+          <Route path="/inventory/" component={InventoryPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </Router>
