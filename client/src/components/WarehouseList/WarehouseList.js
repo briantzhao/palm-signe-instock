0949e1f5685dce899801862a637160ae1ba751bc
migzhasbite <migzhasbite@gmail.com>
2021-11-17 09:49:23 -0500

    PALMSB-6: Applied styling to the Warehouse List component for mobile.

--- client/src/components/WarehouseList/WarehouseList.js
+++ client/src/components/WarehouseList/WarehouseList.js
@@ -2,46 +2,67 @@ import React from "react";
 import { Link } from "react-router-dom";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
+import detailIcon from "../../assets/icons/chevron_right-24px.svg";
+
+import "../WarehouseList/WarehouseList.scss";
 
 export default function WarehouseList({ warehouseList }) {
 	console.log(warehouseList);
 	return (
 		<section className="warehouse">
 			<div className="warehouse__header">
-				<h2 className="warehouse__heading">Warehouses</h2>
+				<h1 className="warehouse__heading">Warehouses</h1>
 				<form className="warehouse__form">
 					<input
 						className="warehouse__input--search"
 						type="search"
-						placeholder="Search"
+						placeholder="Search..."
 					></input>
-					<button className="warehouse__button--add">Add New Warehouse</button>
+					<button className="warehouse__button--add">
+						+ Add New Warehouse
+					</button>
 				</form>
 			</div>
 			<div className="warehouse__container">
-				{warehouseList.map((res) => (
-					<div key={res.id} className="warehouse__card">
-						<div className="warehouse--info">
-							<Link to={`/warehouse/${res.id}`}>
-								<p className="warehouse--name"></p>
-							</Link>
-							<p className="warehouse--address">
-								{res.address}, {res.city}, {res.country}
-							</p>
-							<p className="warehouse--contact-name"></p>
-							<p className="warehouse--contact-info">
-								<br />
-							</p>
-							<div className="warehouse__container--icons">
-								<figure>
-									<img src={deleteIcon} alt="delete icon"></img>
-								</figure>
-								<img src={editIcon} alt="edit icon"></img>
-								<figure></figure>
+				{warehouseList.map(({ id, address, city, country, name, contact }) => (
+					<>
+						<div key={id} className="warehouse__card">
+							<div className="warehouse__container--left">
+								<div className="warehouse__wrapper">
+									<h4 className="warehouse__subheading">WAREHOUSE</h4>
+									<Link to={`/warehouse/${id}`}>
+										<span className="warehouse--name">{name}</span>
+										<img src={detailIcon} alt="detail" />
+									</Link>
+								</div>
+								<div className="warehouse__wrapper">
+									<h4 className="warehouse__subheading">ADDRESS</h4>
+									<p className="warehouse--address">
+										{address}, {city}, {country}
+									</p>
+								</div>
 							</div>
-							<p className="warehouse--contact-info">{res.address}</p>
+							<div className="warehouse__container--right">
+								<div className="warehouse__wrapper">
+									<h4 className="warehouse__subheading">CONTACT NAME</h4>
+									<p className="warehouse--contact-name">{contact.name}</p>
+								</div>
+								<div className="warehouse__wrapper">
+									<h4 className="warehouse__subheading">CONTACT INFORMATION</h4>
+									<p className="warehouse--contact-phone">{contact.phone}</p>
+									<p className="warehouse--contact-email">{contact.email}</p>
+								</div>
+							</div>
 						</div>
-					</div>
+						<div className="warehouse__container--icons">
+							<figure>
+								<img src={deleteIcon} alt="delete icon"></img>
+							</figure>
+							<figure>
+								<img src={editIcon} alt="edit icon"></img>
+							</figure>
+						</div>
+					</>
 				))}
 			</div>
 		</section>
