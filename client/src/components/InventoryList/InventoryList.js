8f7cb8471af7017afeeb55c632fd81ac5f23e8f0
Dean Diaz <dean.diaz001@gmail.com>
2021-11-17 21:36:49 -0500

    modify class names and removed extra code not needed

--- client/src/components/InventoryList/InventoryList.js
+++ client/src/components/InventoryList/InventoryList.js
@@ -18,92 +18,9 @@ export default function InventoryList({ inventoryItems }) {
         />
         <button className="inventory-list__button">+ Add New Item</button>
       </form>
-      {/* {inventoryItems.map(
-        ({
-          id,
-          itemName,
-          category,
-          status,
-          quantity,
-          warehouseID,
-          warehouseName,
-        }) => (
-          <article key={id} className="inventory-list__items">
-            <div className="inventory-list__items-top-container">
-              <section className="inventory-list__items-left-wrap">
-                <div className="inventory-list__items-item-wrap">
-                  <h3 className="inventory-list__items-subheading">
-                    INVENTORY ITEM
-                  </h3>
-                  <Link
-                    to={`/inventories/${id}`}
-                    className="inventory-list__item-link inventory-list__items-item-wrap"
-                    key={id}
-                  >
-                    <span className="inventory-list__items-item">
-                      {itemName}
-                    </span>
-                    <img src={chevron} alt="right arrow icon" />
-                  </Link>
-                </div>
-                <div className="inventory-list__items-item-wrap">
-                  <h3 className="inventory-list__items-subheading">CATEGORY</h3>
-                  <p className="inventory-list__items-category">{category}</p>
-                </div>
-              </section>
-
-              <section className="inventory-list__items-right-wrap">
-                <div className="inventory-list__items-item-wrap">
-                  <h3 className="inventory-list__items-subheading">STATUS</h3>
-                  <p className="inventory-list__items-status">{status}</p>
-                </div>
-
-                <div className="inventory-list__items-item-wrap">
-                  <h3 className="inventory-list__items-subheading">QTY</h3>
-                  <p className="inventory-list__items-qty">{quantity}</p>
-                </div>
-                <div className="inventory-list__items-item-wrap">
-                  <h3 className="inventory-list__items-subheading">
-                    WAREHOUSE
-                  </h3>
-                  <p className="inventory-list__items-warehouse">
-                    {warehouseName}
-                  </p>
-                </div>
-              </section>
-            </div>
-            <div className="inventory-list__items-bottom-container">
-              <figure className="inventory-list-items__icons">
-                <img
-                  src={deleteIcon}
-                  alt="icon of delete action"
-                  className="inventory-list__icons-delete"
-                />
-              </figure>
-              <figure className="inventory-list-items__icons">
-                <img
-                  src={editIcon}
-                  alt="icon of edit action"
-                  className="inventory-list__icons-edit"
-                />
-              </figure>
-            </div>
-          </article>
-        )
-      )} */}
-      {/* <table>
-        <tr className="inventory-list__table-row--mobile">
-          <th className="inventory-list__table-heading">INVENTORY ITEM</th>
-          <th className="inventory-list__table-heading">CATEGORY</th>
-          <th className="inventory-list__table-heading">STATUS</th>
-          <th className="inventory-list__table-heading">QTY</th>
-          <th className="inventory-list__table-heading">WAREHOUSE</th>
-          <th className="inventory-list__table-heading">ACTIONS</th>
-        </tr>
-      </table> */}
 
       <table className="inventory-list__table">
-        <tr className="inventory-list__table-row--mobile">
+        <tr className="inventory-list__table-row">
           <th className="inventory-list__table-heading">INVENTORY ITEM</th>
           <th className="inventory-list__table-heading">CATEGORY</th>
           <th className="inventory-list__table-heading">STATUS</th>
@@ -125,130 +42,74 @@ export default function InventoryList({ inventoryItems }) {
             i
           ) => (
             <>
-              {/* <tbody className="inventory-list__table--large">
-                {i === 0 ? (
-                  <tr className="inventory-list__table-row--mobile">
-                    <th className="inventory-list__table-heading">
-                      INVENTORY ITEM
-                    </th>
-                    <th className="inventory-list__table-heading">CATEGORY</th>
-                    <th className="inventory-list__table-heading">STATUS</th>
-                    <th className="inventory-list__table-heading">QTY</th>
-                    <th className="inventory-list__table-heading">WAREHOUSE</th>
-                    <th className="inventory-list__table-heading">ACTIONS</th>
-                  </tr>
-                ) : (
-                  false
-                )}
-                <tr className="inventory-list__table-row">
-                  <td className="inventory-list__table-data inventory-list__table-data-item">
+              <tbody className="inventory-list__table-body">
+                <tr className="inventory-list__table-row inventory-list__table-row">
+                  <td className="inventory-list__table-data inventory-list__table-item">
                     <Link
                       to={`/inventories/${id}`}
                       className="inventory-list__item-link"
                       key={id}
                     >
                       <span>{itemName}</span>
-                      <img src={chevron} alt="" />
+                      {/* <img src={chevron} alt="right icon link" /> */}
                     </Link>
                   </td>
-                  <td className="inventory-list__table-data inventory-list__table-data-category">
-                    {category}
+                  <td className="inventory-list__table-data inventory-list__table-status">
+                    {status}
                   </td>
 
-                  <td className="inventory-list__table-data ">{status}</td>
-                  <td className="inventory-list__table-data">{quantity}</td>
-                  <td className="inventory-list__table-data">
+                  <td className="inventory-list__table-data inventory-list__table-category">
+                    {category}
+                  </td>
+                  <td className="inventory-list__table-data inventory-list__table-quantity">
+                    {quantity}
+                  </td>
+
+                  <td className="inventory-list__table-data-empty"> </td>
+                  <td
+                    className="inventory-list__table-data 
+                  inventory-list__table-warehouse"
+                  >
                     {warehouseName}
                   </td>
-                </tr> */}
-              {/* <tr className="inventory-list__table-row">
-                  <td className="inventory-list__table-data">
+                  <td
+                    className="inventory-list__table-data--tablet-icons-wrap"
+                    colspan="2"
+                    style={{ width: "100%" }}
+                  >
                     <img
                       src={deleteIcon}
                       alt="delete icon"
-                      className="inventory-list__table-data"
+                      className="inventory-list__table-data-icons--delete"
                     />
-                  </td>
-                  <td className="inventory-list__table-data">
+
                     <img
                       src={editIcon}
                       alt="delete icon"
-                      className="inventory-list__table-data"
+                      className="inventory-list__table-data-icons--edit"
                     />
                   </td>
-                </tr> */}
-              {/* </tbody> */}
-
-              {/* <tbody className="inventory-list__table-body--mobile"> */}
-              <tr className="inventory-list__table-row--mobile">
-                <td className="inventory-list__table-data--mobile inventory-list__table-item--mobile">
-                  <Link
-                    to={`/inventories/${id}`}
-                    className="inventory-list__item-link"
-                    key={id}
+                </tr>
+                <tr className="inventory-list__table-row">
+                  <td
+                    className="inventory-list__table-data-icons-wrap"
+                    colspan="2"
+                    style={{ width: "100%" }}
                   >
-                    <span>{itemName}</span>
-                    {/* <img src={chevron} alt="right icon link" /> */}
-                  </Link>
-                </td>
-                <td className="inventory-list__table-data--mobile inventory-list__table-status--mobile">
-                  {status}
-                </td>
-                {/* </tr> */}
-                {/* <tr className="inventory-list__table-row--mobile"> */}
-                <td className="inventory-list__table-data--mobile inventory-list__table-category--mobile">
-                  {category}
-                </td>
-                <td className="inventory-list__table-data--mobile inventory-list__table-quantity--mobile">
-                  {quantity}
-                </td>
-                {/* </tr> */}
-                {/* <tr className="inventory-list__table-row--mobile"> */}
-                <td className="inventory-list__table-data--mobile-empty"> </td>
-                <td
-                  className="inventory-list__table-data--mobile 
-                  inventory-list__table-warehouse--mobile"
-                >
-                  {warehouseName}
-                </td>
-                <td
-                  className="inventory-list__table-data--tablet-icons-wrap"
-                  colspan="2"
-                  style={{ width: "100%" }}
-                >
-                  <img
-                    src={deleteIcon}
-                    alt="delete icon"
-                    className="inventory-list__table-data--mobile-icons-delete"
-                  />
+                    <img
+                      src={deleteIcon}
+                      alt="delete icon"
+                      className="inventory-list__table-data-icons--delete"
+                    />
 
-                  <img
-                    src={editIcon}
-                    alt="delete icon"
-                    className="inventory-list__table-data--mobile-icons-edit"
-                  />
-                </td>
-              </tr>
-              <tr className="inventory-list__table-row--mobile inventory-list__table-row--mobile-wrap">
-                <td
-                  className="inventory-list__table-data--mobile-icons-wrap"
-                  colspan="2"
-                  style={{ width: "100%" }}
-                >
-                  <img
-                    src={deleteIcon}
-                    alt="delete icon"
-                    className="inventory-list__table-data--mobile-icons-delete"
-                  />
-
-                  <img
-                    src={editIcon}
-                    alt="delete icon"
-                    className="inventory-list__table-data--mobile-icons-edit"
-                  />
-                </td>
-              </tr>
-              {/* </tbody> */}
+                    <img
+                      src={editIcon}
+                      alt="edit icon"
+                      className="inventory-list__table-data-icons-edit"
+                    />
+                  </td>
+                </tr>
+              </tbody>
             </>
           )
         )}
