bffdcbf93f249b30b0becb0299a63b4bd8dab454
Theresemyl <tleung912@gmail.com>
2021-11-17 17:38:44 -0800

    Adds base for edit warehouse form

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer/Footer";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
 import AddWarehouseForm from "./components/AddWarehouseForm/AddWarehouseForm";
+import EditWarehouseForm from "./components/EditWarehouse/EditWarehouseForm";
 // import NotFoundPage from "./pages/NotFoundPage/NotFountPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
@@ -18,6 +19,7 @@ function App() {
             <Route path="/" exact component={WarehousePage} />
             <Route path="/inventory/" component={InventoryPage} />
             <Route path="/add" component={AddWarehouseForm} />
+            <Route path="/warehouses/:id/edit" component={EditWarehouseForm} />
             {/* <Route path="/notfound/" component={NotFoundPage} /> */}
           </Switch>
         </main>
